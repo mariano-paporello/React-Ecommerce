600697e52669a683146027aa90745597624f5d57
Mariano Paporello <mariano.paporello@gmail.com>
2022-07-01 12:23:10 -0300

    03-Tercer desafio  entregable

---
+++ src/components/main/itemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = () => {
+    return ( 
+        <div className="mainPart">
+            <p><h2>Here will be the products</h2></p>
+        </div>
+     );
+}
+ 
+export default ItemListContainer;
