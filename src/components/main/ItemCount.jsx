70f643018ae02f366293c381cb328dfb01a0ca98
Mariano Paporello <mariano.paporello@gmail.com>
2022-07-08 09:30:32 -0300

    Corriguiendo desafio 4

--- src/components/main/ItemCount.jsx
+++ src/components/main/ItemCount.jsx
@@ -9,8 +9,12 @@ const ItemCount = (props) => {
         if(count > props.initial){
             setCount(count -1)
         }
-        
     }
+    function handleAddToCart() {
+        if (props.stock > 0 && count <= props.stock) {
+          props.onAdd(count);
+        }
+      }
     return ( 
         <div className="itemCounter">
             <div >
@@ -20,7 +24,7 @@ const ItemCount = (props) => {
                     <button  onClick={adding}  className="itemCounterButton">+</button>
                 </span>
             </div>
-            <div className="agregarAlCarrito">
+            <div onClick={handleAddToCart} className="agregarAlCarrito">
                 <button> Agregar al carrito</button>
             </div>
         </div>
--- src/components/main/itemListContainer.jsx
+++ src/components/main/itemListContainer.jsx
@@ -1,5 +1,8 @@
 import ItemCount from "./ItemCount.jsx";
 const ItemListContainer = () => {
+    function adding(count){
+        alert(`Se agregaron ${count} productos al carrito`)
+    }
     return ( 
         <div className="mainPart">
             <div className="card">
@@ -10,7 +13,7 @@ const ItemListContainer = () => {
                 </div>
                 <div className="itemCountCss">
                     <h3>Here is the Item counter</h3>
-                    <ItemCount stock={5} initial={1} />
+                    <ItemCount stock={5} initial={1} onAdd={adding} />
                 </div>
             </div>
         </div>
