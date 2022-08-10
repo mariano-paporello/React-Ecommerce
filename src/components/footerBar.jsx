499b3c3cf092012d52b7a14c2fa03657bf1e7d47
Mariano Paporello <mariano.paporello@gmail.com>
2022-08-10 12:49:55 -0300

    12-DoceavoDesafio

--- src/components/footerBar.jsx
+++ src/components/footerBar.jsx
@@ -1,18 +1,37 @@
 const FooterBar = () => {
     return ( 
         <footer className="footerBar">
-           <div> 
-                Social Media:
-                <div></div>
+           <div className="socialMediaPart"> 
+           
+                <div> Social Media:</div>
+                <div className="linksCollection">
+                    <div className="socialMediaLinksDivBorder">
+                        <a href="https://www.instagram.com/buenardiuum_peeu/">
+                                <img src="./images/instagram.png" alt="" />
+                        </a>
+                    </div>
+                    <div className="socialMediaLinksDivBorder"> 
+                        <a href="https://github.com/mariano-paporello">
+                            <img src="./images/github-logo.png" alt="" />
+                        </a>
+                        
+                    </div>
+                    <div className="socialMediaLinksDiv">
+                        <a href="https://www.linkedin.com/in/mariano-paporello-0924b8166/">
+                            <img src="./images/linkedin-icon.png" alt="" />
+                        </a> 
+                    </div>
+                </div>
+            
             </div>
-           <div>
-           Copyright Corte Drinks - 2022. Todos los derechos reservados. Defensa de las y los consumidores.
+           <div className="copyRightPart">
+            Copyright Corte Drinks - 2022. Todos los derechos reservados. Defensa de las y los consumidores.
            </div>
-           <div>
-                Contact Us:
-                <div>
-                    <div></div>
-                    <div></div>
+           <div className="ContactUsPart">
+                Contact Us: 
+                <div className="flex contactUsLinks">
+                    <div className="contactUsLinkBorder"> <a href="mailto:mariano.paporello@gmail.com?Subject=Duda%20o%20consulta%20porfavor%20escribanos%20por%20aqui">Gmail</a></div>
+                    <div><a href="https://wa.me/5491165551861?text=Duda%20o%20consulta%20porfavor%20escribanos%20por%20aqu%C3%AD.">WhatsApp </a></div>
                 </div>
             </div>
         </footer>
